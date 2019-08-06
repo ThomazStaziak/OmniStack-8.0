65e4bcb2c22a7517903732ae97156f949e5c8c60
ThomazStaziak <thostaziak@gmail.com>
2019-08-06 17:13:00 -0300

    adding the dislike controller

---
+++ backend/src/controllers/DislikeController.js
@@ -0,0 +1,20 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { id } = req.params;
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(id);
+
+    if (!targetDev)
+      return res.status(400).send({ message: "Dev does not exists" });
+
+    loggedDev.dislikes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+};
