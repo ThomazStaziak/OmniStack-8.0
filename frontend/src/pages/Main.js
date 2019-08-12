788ed37e0e6301d41e7fcb46e192e2e324119671
ThomazStaziak <thostaziak@gmail.com>
2019-08-12 17:11:51 -0300

    adding match in main.js

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import io from 'socket.io-client';
 
 import './Main.css';
 
@@ -7,10 +8,12 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 
 export default function Main({ match }) {
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     async function loadUsers() {
@@ -25,6 +28,16 @@ export default function Main({ match }) {
 
     loadUsers();
   }, [match.params.id]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+        query: { user: match.params.id }
+    });
+
+    socket.on('match', dev => {
+        setMatchDev(dev);
+    })
+  }, [match.params.id])
 
   async function handleLike(id) {
     await api.post(`/devs/${id}/like`, null, {
@@ -45,6 +58,8 @@ export default function Main({ match }) {
 
     setUsers(users.filter(user => user._id !== id));
   }
+
+
 
   return (
     <div className="main-container">
@@ -83,6 +98,16 @@ export default function Main({ match }) {
         ) : (
             <div className="empty">
                 Nenhum desenvolvedor está disponível :(
+            </div>
+        ) }
+
+        { matchDev && (
+            <div className="match-container">
+                <img src={itsamatch} alt="It's a match"/>
+                  <img className="avatar" src={matchDev.avatar} alt={matchDev.name}/>
+                <strong>{matchDev.name}</strong>
+                <p>{matchDev.bio}</p>
+                <button type="button" onClick={() => setMatchDev(null)}>fechar</button>
             </div>
         ) }
     </div>
