e17d38f896927cadde143758469962ffeae834b0
ThomazStaziak <thostaziak@gmail.com>
2019-08-12 17:10:40 -0300

    enabling the io listen

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,16 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+});
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${
@@ -17,6 +25,13 @@ mongoose.connect(
     useNewUrlParser: true
   }
 );
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 
 app.use(express.json());
 app.use(cors());
